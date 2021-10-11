77d000feb8ac6accb0e267491f4e31a0c25396ce
dimas11 <www.dimas11@mail.ru>
2021-10-11 11:07:35 +1000

    47lesson

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,7 +6,7 @@ import Post from './Post/Post';
 const MyPosts = (props) => {
 
     let likePlus=()=>  {
-        props.like();
+        props.likePlus();
     }
 
     let postsElements =
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -16,25 +16,32 @@ let initialState = {
 const profileReducer = (state = initialState, action) => {
 
     switch(action.type) {
-        case ADD_POST:
+        case ADD_POST: {
             let newPost = {
                 id: 5,
                 message: state.newPostText,
                 likesCount: 0
             };
-            state.posts.push(newPost);
-            state.newPostText = '';
-            return state;
-        case UPDATE_NEW_POST_TEXT:
-            state.newPostText = action.newText;
-            return state;
-        case DEL_POST:
-            state.posts.pop();
-            return state;
-        case LIKE_POST:
+            let stateCopy={...state}
+            stateCopy.posts=[...state.posts]
+            stateCopy.posts.push(newPost);
+            stateCopy.newPostText = '';
+            return stateCopy;}
+        case UPDATE_NEW_POST_TEXT:{
+            let stateCopy={...state}
+            stateCopy.newPostText = action.newText;
+            return stateCopy;}
+        case DEL_POST:{
+            let stateCopy={...state}
+            stateCopy.posts=[...state.posts]
+            stateCopy.posts.pop();
+            return  stateCopy;}
+        case LIKE_POST:{
+            let stateCopy={...state}
+            stateCopy.posts=[...state.posts]
             let id;
-            state.posts[1].likesCount+=1;
-            return state;
+            stateCopy.posts[1].likesCount+=1;
+            return  stateCopy;}
         default:
             return state;
     }
